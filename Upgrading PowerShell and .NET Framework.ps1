54fa62abb3fcb76e4f378cf57f5c3b92641925b3
vipin-k <vipinkumargupta@gmail.com>
2018-06-22 13:36:08 -0700

    powershell1.

---
+++ Upgrading PowerShell and .NET Framework.ps1
@@ -0,0 +1,10 @@
+﻿$url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Upgrade-PowerShell.ps1"
+$file = "$env:temp\Upgrade-PowerShell.ps1"
+$username = "Administrator"
+$password = "Password"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
+# version can be 3.0, 4.0 or 5.1
+&$file -Version 5.1 -Username $username -Password $password -Verbose
